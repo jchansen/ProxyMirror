6b182390552fcf580ad3bbd4cb779e34b65d643b
Jason Hansen <jason_hansen@outlook.com>
2013-08-27 20:48:55 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProxyMirror::Application.config.secret_key_base = '8533582bf5c4b201f60a920ee0a433c712aa85d8e7a1e99cb6fc7aca0d760273c826d9b32f41f107978d852e111247759aee8f9556534a393b12a97ff2f45a21'
